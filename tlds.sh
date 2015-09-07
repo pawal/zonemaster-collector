79d155bd53d62ea325c9b765315691faf04b428d
Patrik Wallstrom <pawal@blipp.com>
2015-09-07 16:01:13 +0200

    remove the root '.'

--- tlds.sh
+++ tlds.sh
@@ -4,4 +4,4 @@
 # curl -s http://www.internic.net/domain/root.zone
 # or as plain text: http://data.iana.org/TLD/tlds-alpha-by-domain.txt
 
-dig . axfr @xfr.lax.dns.icann.org | awk '$4=="NS" { print $1}' | sort -u
+dig . axfr @xfr.lax.dns.icann.org | awk '$4=="NS" { print $1}' | sort -u | grep -v ^.$
